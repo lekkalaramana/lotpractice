ef8cb124f45a0afa6ce8a8c2f66aa7c2f1def0a9
lekkalaramana <ramana.lekkal@gmail.com>
2019-08-07 11:30:08 +0530

    second commit

---
+++ config/deploy/production.rb
@@ -0,0 +1,46 @@
+# set :branch, 'testing'
+# set :stage, :development
+# # Setting RAILS_ENV environment variable on server
+# set :rails_env, :development
+# set :deploy_to, '/var/www/flights-dynamic'
+# set :normalize_asset_timestamps, %{public/images public/javascripts public/stylesheets}
+
+# role :app, %w{ubuntu@54.254.252.106}
+
+# set :ssh_options, {
+#    forward_agent: true,
+#    user: "ubuntu",
+#    :keepalive => true,
+#    :keepalive_interval => 3000
+# }
+# set :user, "ubuntu"
+# # Force rake through bundle exec
+# SSHKit.config.command_map[:rake] = "bundle exec rake"
+
+# # Force rails through bundle exec
+# SSHKit.config.command_map[:rails] = "bundle exec rails"
+
+
+
+# production deploy server = 52.77.242.232
+
+# testing deploy server = 54.169.165.81
+# ssh ubuntu@54.169.165.81
+
+# ssh ubuntu@52.77.242.232
+
+
+
+server '54.254.252.106',
+ user: 'ubuntu',
+ roles: %w{app web app},
+ ssh_options: {
+     forward_agent: true,
+ }
+
+set :stage, :production
+set :branch, :development
+set :ssh_options, {:forward_agent => true}
+
+# Setting RAILS_ENV environment variable on server
+set :rails_env, :production
